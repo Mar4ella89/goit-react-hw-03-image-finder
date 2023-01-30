1cf69881518404e9b27b65c3d4d38fb0a80ff982
Marat Baikin <3371335@gmail.com>
2023-01-31 01:17:00 +0200

    api

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,5 +1,4 @@
 import { Component } from 'react';
-import React from 'react';
 import axios from 'axios';
 import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
 
@@ -12,24 +11,30 @@ class ImageGallery extends Component {
   };
 
   componentDidUpdate(prevProps, prevState) {
-    const prevSearchQuery = prevProps.searchQuery;
-    const nextSearchQuery = this.props.searchQuery;
-    if (prevSearchQuery !== nextSearchQuery) {
-      axios
-        .get(
+        const prevSearchQuery = prevProps.searchQuery;
+        const nextSearchQuery = this.props.searchQuery;
+        if (prevSearchQuery !== nextSearchQuery) {
+    axios.get(
           `https://pixabay.com/api/?q=${nextSearchQuery}&page=1&key=31981261-43107a8c97a37675e78f6a341&image_type=photo&orientation=horizontal&per_page=12`
         )
         .then(({ data }) => {
           this.setState({ items: data.hits });
           console.log(data.hits);
+        
         });
-    }
   }
+}
 
   render() {
-    return <ul className={css.ImageGallery}>
-        <ImageGalleryItem/>
-    </ul>;
+    const { items } = this.state;
+    const elements = items.map(({ id, webformatURL }) => (
+      <ImageGalleryItem key={id} srcImg={webformatURL} />
+    ));
+    return (
+        <ul className={css.ImageGallery}>
+            {elements}
+        </ul>
+    );
   }
 }
 
--- src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import css from './ImageGalleryItem.module.css';
 
-const ImageGalleryItem = ({id, webformatURL}) => {
+const ImageGalleryItem = ({srcImg}) => {
   return (
-    <li key={id} className={css.galleryItem}>
-      <img src={webformatURL} alt="" className={css.ImageGalleryItemImage}/>
+    <li className={css.galleryItem}>
+      <img src={srcImg} alt="" className={css.ImageGalleryItemImage}/>
     </li>
   );
 };
