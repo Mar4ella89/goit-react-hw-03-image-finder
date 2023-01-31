19e70605f6f67e8f1af4a11846142ee24ddc3856
Marat Baikin <3371335@gmail.com>
2023-01-31 21:35:34 +0200

    change app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,11 @@
 import { Component } from 'react';
 import { ToastContainer } from 'react-toastify';
+import Loader from 'components/Loader/Loader';
 
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
+import searchQueryImg from 'services/gallery-api';
+import Button from 'components/Button/Button';
 
 import 'react-toastify/dist/ReactToastify.css';
 import css from './App.module.css';
@@ -10,18 +13,53 @@ import css from './App.module.css';
 export class App extends Component {
   state = {
     searchQuery: '',
+    items: [],
+    loading: false,
+    error: null,
+    page: 1,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const{searchQuery, page} = this.state
+    const prevSearchQuery = prevState.searchQuery;
+        const prevPage = prevState.page;
+        
+    if (prevSearchQuery !== searchQuery || prevPage !== page) {
+      this.setState({ loading: true });
+
+      searchQueryImg(searchQuery, page)
+        .then(data => {
+          this.setState(({ items }) => ({ items: [...items, ...data.hits] }));
+        })
+        .catch(error => {
+          this.setState({ error: error.message });
+        })
+        .finally(() => this.setState({ loading: false }));
+    }
+  }
+
+  loadMore = () => {
+    this.setState(({ page }) => ({ page: page + 1 }));
   };
 
   handleFormSubmit = searchQuery => {
-    this.setState({ searchQuery });
+    this.setState({ searchQuery, items: [], page: 1});
   };
 
   render() {
+    const { items, loading, error } = this.state;
     return (
       <div className={css.App}>
         <Searchbar onSubmit={this.handleFormSubmit} />
         <ToastContainer autoClose={3000} />
-        <ImageGallery searchQuery={this.state.searchQuery} />
+        <ImageGallery items={items} />
+
+        {loading && <Loader />}
+        {error && <p>An error has occurred. Please try again later...</p>}
+
+        {Boolean(items.length) && (
+          <Button text={'Load more'} onClick={this.loadMore} />
+        )}
       </div>
     );
   }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,69 +1,20 @@
-import Loader from 'components/Loader/Loader';
-// import { toast } from 'react-toastify';
-// import axios from 'axios';
-
-import searchQueryImg from 'services/gallery-api';
-
-import { Component } from 'react';
+import React from 'react';
 import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
-import Button from 'components/Button/Button';
+
 
 import css from './ImageGallery.module.css';
 
-class ImageGallery extends Component {
-  state = {
-    searchQuery: '',
-    items: [],
-    loading: false,
-    error: null,
-    page: 1,
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-    const prevSearchQuery = prevProps.searchQuery;
-    const nextSearchQuery = this.props.searchQuery;
-    const nextPage = this.state.page;
-    const prevPage = prevState.page;
-    if (prevSearchQuery !== nextSearchQuery || prevPage !== nextPage) {
-      this.setState({ loading: true });
-
-      // axios
-      //   .get(
-      //     `https://pixabay.com/api/?q=${nextSearchQuery}&page=1&key=31981261-43107a8c97a37675e78f6a341&image_type=photo&orientation=horizontal&per_page=12`
-      //   )
-
-      searchQueryImg(nextSearchQuery, nextPage)
-        .then(data => {
-          this.setState(({ items }) => ({ items: [...items, ...data.hits] }));
-        })
-        .catch(error => {
-          this.setState({ error: error.message });
-        })
-        .finally(() => this.setState({ loading: false }));
-    }
-  }
-
-  loadMore = () => {
-    this.setState(({ page }) => ({ page: page + 1 }));
-  };
-
-  render() {
-    const { items, loading, error } = this.state;
+  const ImageGallery = ({items})=> {
+    // const { items, loading, error } = this.state;
     const elements = items.map(({ id, webformatURL }) => (
       <ImageGalleryItem key={id} srcImg={webformatURL} />
     ));
     return (
-      <>
+     
         <ul className={css.ImageGallery}>{elements}</ul>
-
-        {loading && <Loader />}
-        {error && <p>An error has occurred. Please try again later...</p>}
-        {Boolean(items.length) && (
-          <Button text={'Load more'} onClick={this.loadMore} />
-        )}
-      </>
+        
     );
   }
-}
 
+ 
 export default ImageGallery;
