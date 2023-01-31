2782db11b336e13baf024c7c2015df0adfa57a73
Marat Baikin <3371335@gmail.com>
2023-01-31 22:57:25 +0200

    upd

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
-
+import PropTypes from 'prop-types';
 
 import css from './ImageGallery.module.css';
 
-  const ImageGallery = ({items})=> {
-    // const { items, loading, error } = this.state;
-    const elements = items.map(({ id, webformatURL }) => (
-      <ImageGalleryItem key={id} srcImg={webformatURL} />
-    ));
-    return (
-     
-        <ul className={css.ImageGallery}>{elements}</ul>
-        
-    );
-  }
+const ImageGallery = ({ items }) => {
+  const elements = items.map(({ id, webformatURL }) => (
+    <ImageGalleryItem key={id} srcImg={webformatURL} />
+  ));
+  return <ul className={css.ImageGallery}>{elements}</ul>;
+};
 
- 
+ImageGallery.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      webformatURL: PropTypes.string.isRequired,
+    })
+  ),
+};
+
 export default ImageGallery;
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import css from './Modal.module.css';
+
+const Modal = ()=>{
+    return (<div className={css.Overlay}>
+    <div className={css.Modal}>
+      <img src="" alt="" />
+    </div>
+  </div>)
+}
+
+export default Modal
